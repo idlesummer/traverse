1bebb77ffeb28af64594d194b0070f19da8b51dd
idlesummer <09louisthebob@gmail.com>
2026-08-17 03:16:33 +0800

    chore: configure oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  ignorePatterns: [],
+
+  // Formatting
+  trailingComma: 'all',
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: false,
+  endOfLine: 'lf',
+  insertFinalNewline: true,
+})
