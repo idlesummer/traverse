f0de4470b34f17e75c2bcac8df8892279ba1a2b0
idlesummer <09louisthebob@gmail.com>
2026-08-17 21:20:08 +0800

    chore: setup stylistic eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,26 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   tseslint.configs.recommended,
+  {
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+      '@stylistic/eol-last': ['warn', 'always'],
+      '@stylistic/object-curly-spacing': ['warn', 'always'],
+      '@stylistic/quotes': ['warn', 'single', { avoidEscape: true }],
+      '@stylistic/semi': ['warn', 'never'],
+    },
+  },
 ]);
