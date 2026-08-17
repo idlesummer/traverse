b3c7f904074322d9f4f41ea748a9c70c2438eb39
idlesummer <09louisthebob@gmail.com>
2026-08-17 21:28:54 +0800

    chore: setup for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: {
       globals: globals.browser,
     },
@@ -24,4 +24,4 @@ export default defineConfig([
       '@stylistic/semi': ['warn', 'never'],
     },
   },
-]);
+])
